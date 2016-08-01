2e0325b7f6f9a28c4684763b03908391293d508b
diemmarkus <diemmarkus@gmail.com>
2016-08-01 17:29:01 +0200

    Text block segmentation (stub)

---
+++ src/Module/TextBlockSegmentation.h
@@ -0,0 +1,89 @@
+/*******************************************************************************************************
+ ReadFramework is the basis for modules developed at CVL/TU Wien for the EU project READ. 
+  
+ Copyright (C) 2016 Markus Diem <diem@caa.tuwien.ac.at>
+ Copyright (C) 2016 Stefan Fiel <fiel@caa.tuwien.ac.at>
+ Copyright (C) 2016 Florian Kleber <kleber@caa.tuwien.ac.at>
+
+ This file is part of ReadFramework.
+
+ ReadFramework is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ ReadFramework is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ The READ project  has  received  funding  from  the European  Union’s  Horizon  2020  
+ research  and innovation programme under grant agreement No 674943
+ 
+ related links:
+ [1] http://www.caa.tuwien.ac.at/cvl/
+ [2] https://transkribus.eu/Transkribus/
+ [3] https://github.com/TUWien/
+ [4] http://nomacs.org
+ *******************************************************************************************************/
+
+#pragma once
+
+#include "BaseModule.h"
+#include "Pixel.h"
+
+#pragma warning(push, 0)	// no warnings from includes
+// Qt Includes
+#pragma warning(pop)
+
+#ifndef DllModuleExport
+#ifdef DLL_MODULE_EXPORT
+#define DllModuleExport Q_DECL_EXPORT
+#else
+#define DllModuleExport Q_DECL_IMPORT
+#endif
+#endif
+
+// Qt defines
+
+namespace rdf {
+
+// read defines
+
+
+class DllModuleExport TextBlockConfig : public ModuleConfig {
+
+public:
+	TextBlockConfig();
+
+	virtual QString toString() const override;
+
+protected:
+
+	//void load(const QSettings& settings) override;
+	//void save(QSettings& settings) const override;
+};
+
+class DllModuleExport TextBlock : public Module {
+
+public:
+	TextBlock(const QVector<Pixel>& superPixels = QVector<Pixel>());
+
+	bool isEmpty() const override;
+	bool compute() override;
+	
+	cv::Mat displayImage() const;
+
+	QString toString() const override;
+
+private:
+	QVector<Pixel> mSuperPixels;
+	cv::Mat mDisplayImage;
+
+	bool checkInput() const override;
+};
+
+};
