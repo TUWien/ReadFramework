65e30d91c68fc6b92dc7ff479a0b905165153987
Stefan Fiel <stefan@nomacs.org>
2016-08-10 13:20:21 +0200

    added NDEBUG flag

--- cmake/Unix.cmake
+++ cmake/Unix.cmake
@@ -6,5 +6,6 @@ if(CMAKE_BUILD_TYPE STREQUAL "debug" OR CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMA
 	add_definitions(-DDEBUG)
 else()
 	message(STATUS "A release build (non-debug). Debugging outputs are silently ignored.")
-	#add_definitions(-DQT_NO_DEBUG_OUTPUT)
+	add_definitions(-DQT_NO_DEBUG_OUTPUT)
+	add_definitions(-DNDEBUG)
 endif()
