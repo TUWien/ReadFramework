17c7426e885bffc7b54a377dd7ec53a6097ac292
diemmarkus <diemmarkus@gmail.com>
2017-01-16 11:08:44 +0100

    readAttributes function added for custom attributes in PAGE XML files

--- src/Core/Elements.h
+++ src/Core/Elements.h
@@ -115,6 +115,7 @@ public:
 	virtual QString childrenToString() const;
 
 	virtual bool read(QXmlStreamReader& reader);
+	virtual void readAttributes(QXmlStreamReader& reader);
 	virtual void write(QXmlStreamWriter& writer, bool withChildren = true, bool close = true) const;
 	virtual void writeChildren(QXmlStreamWriter& writer) const;
 
@@ -143,6 +144,8 @@ public:
 	void setText(const QString& text);
 	QString text() const;
 
+	
+	virtual void readAttributes(QXmlStreamReader& reader) override;
 	virtual bool read(QXmlStreamReader& reader) override;
 	virtual void write(QXmlStreamWriter& writer, bool withChildren = true, bool close = true) const override;
 
